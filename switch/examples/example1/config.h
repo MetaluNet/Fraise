9dac84b73e816abc130d105db44e8d1758ce51e8
Antoine Rousseau <antoine@metalu.net>
2016-01-13 00:40:38 +0100

    Add example to switch module

---
+++ switch/examples/example1/config.h
@@ -0,0 +1,7 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#define ANALOG_FILTER 3 
+
+#endif // _CONFIG_H_
+
---
+++ switch/examples/example1/main.c
@@ -0,0 +1,73 @@
+/*********************************************************************
+ *               switch example for Versa1.0
+ *	Switch capture on connectors K1, K2, K3 and K4. 
+ *********************************************************************/
+
+#define BOARD Versa1
+
+#include <fruit.h>
+#include <switch.h>
+
+void main(void)
+{	
+	t_delay mainDelay;
+	
+//----------- Setup ----------------
+	fruitInit();
+			
+	pinModeDigitalOut(LED); 	// set the LED pin mode to digital out
+	digitalClear(LED);		// clear the LED
+	delayStart(mainDelay, 5000); 	// init the mainDelay to 5 ms
+
+//----------- Switch setup ----------------
+	switchInit();		// init switch module
+	switchSelect(0,K1);	// assign connector K1 to switch channel 0
+	switchSelect(1,K2);
+	switchSelect(2,K3);
+	switchSelect(3,K4);
+
+// ---------- Main loop ------------
+	while(1){
+		fraiseService();	// listen to Fraise events
+		switchService();	// switch management routine
+
+		if(delayFinished(mainDelay)) // when mainDelay triggers :
+		{
+			switchSend();		// send switch channels that changed
+		}
+	}
+}
+
+// Receiving
+
+void fraiseReceiveChar() // receive text
+{
+	unsigned char c;
+	
+	c=fraiseGetChar();
+	if(c=='L'){		//switch LED on/off 
+		c=fraiseGetChar();
+		digitalWrite(LED, c!='0');		
+	}
+	else if(c=='E') { 	// echo text (send it back to host)
+		printf("C");
+		c = fraiseGetLen(); 			// get length of current packet
+		while(c--) printf("%c",fraiseGetChar());// send each received byte
+		putchar('\n');				// end of line
+	}	
+}
+
+void fraiseReceive() // receive raw bytes
+{}
+
+void fraiseReceiveCharBroadcast(){} // receive broadcast text
+void fraiseReceiveBroadcast(){} // receive broadcast raw bytes
+
+
+// Interrupt routines
+void highInterrupts(){}
+void lowInterrupts(){}
+
+// EEPROM parameters management (see eeparams module doc)
+void EEdeclareMain(){}
+
