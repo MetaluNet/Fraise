656652d5371c8420f47af22b2e2c9f11fe016bc6
Antoine Rousseau <_antoine_@metalu.net>
2018-12-18 11:50:55 +0100

    improve bootloaders compilation script

--- bootloader/18f/compile.sh
+++ bootloader/18f/compile.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+DONE=
+NOLKR=
+COMPILEERROR=
+
 if [ x$1 = x ] ; then
 	#echo usage: $0 boardname
 	#exit
-	for b in ../../boards/* ; do 
+	for b in ../../boards/*/ ; do 
+		export BOARD=`basename $b`
+		echo board=$BOARD
 		if test -f $b/board.lkr ; then
-			echo board=$b
-			export BOARD=`basename $b`
-			echo $BOARD
-			if ! make ; then exit; fi
-		fi
+			if make &> /dev/null ; then DONE+=$BOARD" " ; else COMPILEERROR+=$BOARD" " ; fi
+		else NOLKR+=$BOARD" " ; fi
 	done
+	
+	echo "boards missing board.lkr: " $NOLKR
+	echo "boards with compilation error: " $COMPILEERROR
+	echo "boards successfully compiled: " $DONE 
 	exit
 fi
 
