9ade5b5c6afa1fcf158a4a92d1bfd2f07d36baad
Antoine Rousseau <_antoine_@metalu.net>
2021-01-07 14:03:45 +0100

    publish-deken: remove 'Sources' pseudo-arch from package

--- doc/publish-deken.sh
+++ doc/publish-deken.sh
@@ -14,5 +14,11 @@ cd doc/publish-deken
 
 echo $VERSION > Fraise/VERSION.txt
 
-deken upload --version $VERSION Fraise
+deken package --version $VERSION Fraise
+
+mv "Fraise[v$VERSION](Sources).dek"        "Fraise[v$VERSION].dek"
+mv "Fraise[v$VERSION](Sources).dek.asc"    "Fraise[v$VERSION].dek.asc"
+mv "Fraise[v$VERSION](Sources).dek.sha256" "Fraise[v$VERSION].dek.sha256"
+
+deken upload --no-source-error "Fraise[v$VERSION].dek"
 
