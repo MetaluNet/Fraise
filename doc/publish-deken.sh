dbe6273d9895f85f3b68cf1cb7e3c6e532680004
Antoine Rousseau <_antoine_@metalu.net>
2021-01-07 12:45:49 +0100

    finish (for now...) the publish-deken script

--- doc/publish-deken.sh
+++ doc/publish-deken.sh
@@ -1,12 +1,18 @@
 #!/bin/bash -e
 
+#to be called from the doc/ directory (here)
+
 VERSION=`git describe`
 
 rm -Rf publish-deken/
 mkdir -p publish-deken/
 cd ..
-#git archive HEAD > doc/publish-deken/Fraise.tar
+
 git archive --prefix=Fraise/ HEAD | (cd doc/publish-deken && tar xf -)
+
 cd doc/publish-deken
+
+echo $VERSION > Fraise/VERSION.txt
+
 deken upload --version $VERSION Fraise
 
