9b219254fdd1849060b7ccd072e9801fcec53040
Antoine Rousseau <_antoine_@metalu.net>
2024-02-07 12:12:55 +0100

    pico: update ws2812 example

--- pico/ws2812/example/fw_example_ws2811/main.c
+++ pico/ws2812/example/fw_example_ws2811/main.c
@@ -13,11 +13,32 @@
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
 #include "ws2812.h"
+#include "fraise.h"
 
-#define IS_RGBW true
+#define IS_RGBW false
 #define NUM_PIXELS 150
 
-#define put_pixel ws2812_put_pixel
+//#define put_pixel ws2812_put_pixel
+
+uint32_t framebuffer[NUM_PIXELS];
+int fb_index;
+
+void set_pixel(int n, uint8_t r, uint8_t g, uint8_t b) {
+	if(n >= 0 && n < NUM_PIXELS) {
+		framebuffer[n] = urgb_u32(r, g, b);
+	}
+}
+
+void put_pixel(uint32_t pixel_rgb) {
+	if(fb_index < NUM_PIXELS) {
+		framebuffer[fb_index++] = pixel_rgb;
+	}
+}
+
+void framebuffer_send() {
+	for(int i = 0; i < NUM_PIXELS; i++)
+		ws2812_put_pixel(framebuffer[i]);
+}
 
 void pattern_snakes(uint len, uint t) {
     for (uint i = 0; i < len; ++i) {
@@ -75,21 +96,49 @@ int pat;
 int dir;
 int t = 0;
 int i = 1000;
+bool pattern_play = false;
+bool pattern_rnd = false;
 
 bool tick_callback(repeating_timer_t *rt)
 {
-	if(i++ >= 1000) {
-        pat = rand() % count_of(pattern_table);
-        dir = (rand() >> 30) & 1 ? 1 : -1;
-        i = 0;
+	if(pattern_play) {
+		if(i++ >= 1000) {
+			if(pattern_rnd) {
+				pat = rand() % count_of(pattern_table);
+				dir = (rand() >> 30) & 1 ? 1 : -1;
+			}
+			i = 0;
+		}
+		fb_index = 0;
+		pattern_table[pat].pat(NUM_PIXELS, t);
+		t += dir;
 	}
-	pattern_table[pat].pat(NUM_PIXELS, t);
-	t += dir;
+	//framebuffer_send();
+	ws2812_dma_transfer(framebuffer, NUM_PIXELS);
 	return true;
 }
 
 void setup() {
-    ws2812_setup(IS_RGBW);
-	add_repeating_timer_ms(10, tick_callback, NULL, &tick_timer);
+	ws2812_setup(IS_RGBW);
+	add_repeating_timer_ms(5, tick_callback, NULL, &tick_timer);
+}
+
+void fraise_receivebytes(const char* data, uint8_t len) {
+	char command = data[0];
+	switch(command) {
+		case 20: set_pixel((int)data[1], data[2], data[3], data[4]); break;
+		case 21:
+			pattern_play = data[1] != 0;
+			pattern_rnd = data[2] != 0;
+			pat = data[3];
+			dir = data[4] != 0 ? 1 : -1;
+			break;
+	}
+}
+
+void fraise_receivechars(const char *data, uint8_t len){
+	if(data[0] == 'E') { // Echo
+		printf("E%s\n", data + 1);
+	}
 }
 
