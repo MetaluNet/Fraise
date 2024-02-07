c2162e58b210e914c5b9bc92697be7694290afe5
Antoine Rousseau <_antoine_@metalu.net>
2024-02-07 12:10:37 +0100

    pico ws2812: add dma transfer function

--- pico/ws2812/ws2812.c
+++ pico/ws2812/ws2812.c
@@ -10,8 +10,9 @@
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
-#include "fraise.h"
+#include "hardware/dma.h"
 
+#include "fraise.h"
 #include "ws2812.pio.h"
 
 #ifdef PICO_DEFAULT_WS2812_PIN
@@ -26,15 +27,40 @@ static uint sm;
 static uint pgm_offset; // Offset of the program in the pio
 //static uint pio_irq;  // The irq used by the sm
 //static uint irq_index;  // The offset of pio_irq relatively to PIO0_IRQ_0 or PIO1_IRQ_0
+int dma_chan = -1;
+dma_channel_config dma_config;
 
-void ws2812_put_pixel(uint32_t pixel_grb) {
-    pio_sm_put_blocking(pio, sm, pixel_grb << 8u);
+void ws2812_put_pixel(uint32_t pixel_rgb) {
+    pio_sm_put_blocking(pio, sm, pixel_rgb);
 }
 
-void ws2812_setup(bool rgbw) {
+bool ws2812_setup(bool rgbw) {
     if (!claim_pio_sm_irq(&ws2812_program, &pio, &sm, &pgm_offset, NULL /*&pio_irq*/)) {
-        panic("failed to setup pio");
+        //panic("failed to setup pio");
+        return false;
     }
     ws2812_program_init(pio, sm, pgm_offset, WS2812_PIN, 800000, rgbw);
+    return true;
+}
+
+void ws2812_dma_transfer(uint32_t *pixels, int num) {
+    // Get a free channel, panic() if there are none
+    if(dma_chan == -1) {
+        dma_chan = dma_claim_unused_channel(true);
+        dma_config = dma_channel_get_default_config(dma_chan);
+        channel_config_set_transfer_data_size(&dma_config, DMA_SIZE_32);
+        channel_config_set_read_increment(&dma_config, true);
+        channel_config_set_write_increment(&dma_config, false);
+        channel_config_set_dreq(&dma_config, pio_get_dreq(pio, sm, true));
+    }
+
+    dma_channel_configure(
+        dma_chan,          // Channel to be configured
+        &dma_config,            // The configuration we just created
+        &pio->txf[sm], // The write address
+        pixels,        // The initial read address
+        num,           // Number of transfers
+        true           // Start immediately.
+    );
 }
 
--- pico/ws2812/ws2812.h
+++ pico/ws2812/ws2812.h
@@ -13,15 +13,17 @@
  *  \ingroup pico
  */
 
-void ws2812_setup(bool rgbw);
+bool ws2812_setup(bool rgbw);
 
-void ws2812_put_pixel(uint32_t pixel_grb);
+void ws2812_put_pixel(uint32_t pixel_rgb);
 
 static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
     return
-            ((uint32_t) (r) << 8) |
+            ((uint32_t) (r) << 24) |
             ((uint32_t) (g) << 16) |
-            (uint32_t) (b);
+            ((uint32_t) (b) << 8 );
 }
+
+void ws2812_dma_transfer(uint32_t *pixels, int num);
 
 #endif
