b05c668e41300c0aef12026ddbe685403f0e72e1
Antoine Rousseau <_antoine_@metalu.net>
2024-03-29 11:35:10 +0100

    codestyle again

--- pico/sys/usb_bootloader/blink/main.c
+++ pico/sys/usb_bootloader/blink/main.c
@@ -15,29 +15,29 @@ int ledPeriod = 250;
     gpio_put(LED_PIN, 1);
 }*/
 
-void loop(){
-	static absolute_time_t nextLed;// = make_timeout_time_ms(100);
-	static bool led = false;
+void loop() {
+    static absolute_time_t nextLed;// = make_timeout_time_ms(100);
+    static bool led = false;
 
-	if(time_reached(nextLed)) {
-		//if(absolute_time_min(nextLed, get_absolute_time()) == nextLed) {
-		gpio_put(LED_PIN, led = !led);
-		nextLed = make_timeout_time_ms(ledPeriod);
-	}
+    if(time_reached(nextLed)) {
+        //if(absolute_time_min(nextLed, get_absolute_time()) == nextLed) {
+        gpio_put(LED_PIN, led = !led);
+        nextLed = make_timeout_time_ms(ledPeriod);
+    }
 }
 
-void fraise_receivebytes(const char *data, uint8_t len){
-	if(data[0] == 1) ledPeriod = (int)data[1] * 10;	
-	else {
-		printf("rcvd ");
-		for(int i = 0; i < len; i++) printf("%d ", (uint8_t)data[i]);
-		putchar('\n');
-	}
+void fraise_receivebytes(const char *data, uint8_t len) {
+    if(data[0] == 1) ledPeriod = (int)data[1] * 10;
+    else {
+        printf("rcvd ");
+        for(int i = 0; i < len; i++) printf("%d ", (uint8_t)data[i]);
+        putchar('\n');
+    }
 }
 
-void fraise_receivechars(const char *data, uint8_t len){
-	if(data[0] == 'E') { // Echo
-		printf("E%s\n", data + 1);
-	}
+void fraise_receivechars(const char *data, uint8_t len) {
+    if(data[0] == 'E') { // Echo
+        printf("E%s\n", data + 1);
+    }
 }
 
--- pico/sys/usb_bootloader/make_board_bootloader.sh
+++ pico/sys/usb_bootloader/make_board_bootloader.sh
@@ -3,8 +3,8 @@
 CMAKE=../../../../Fraise-toolchain/cmake/bin/cmake
 
 if [ x$1 == x ] ; then
-	echo ERROR: no board specified!
-	exit
+    echo ERROR: no board specified!
+    exit
 fi
 
 $CMAKE -B build -S . -Dfraise_board=$1
