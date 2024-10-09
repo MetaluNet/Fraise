0b58f232b2803e6f3fe78eba4aede84ccc61a6d3
Antoine Rousseau <_antoine_@metalu.net>
2024-10-09 15:24:25 +0200

    make ws2812 C compatible

--- pico/ws2812/ws2812.h
+++ pico/ws2812/ws2812.h
@@ -13,6 +13,10 @@
  *  \ingroup pico
  */
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 bool ws2812_setup(int pin, bool rgbw);
 
 void ws2812_put_pixel(uint32_t pixel_rgb);
@@ -25,5 +29,9 @@ static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
 }
 
 void ws2812_dma_transfer(uint32_t *pixels, int num);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
