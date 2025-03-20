839be9f4761dbecac5ce2c07fb548a654fd02b66
Antoine Rousseau <_antoine_@metalu.net>
2025-03-20 14:38:37 +0100

    add new board pico_drv1 with DRV pin active level 1 instead of 0

---
+++ boards/pico_drv1/boardconfig.h
@@ -0,0 +1,13 @@
+//
+//  Config for RaspberryPi pico board
+//
+
+// ------------- Chip / pins : -----------------
+#define CHIP rp2040
+#define FAMILLY rp2040
+
+#define FRAISE_RX_PIN  20
+#define FRAISE_TX_PIN  21
+#define FRAISE_DRV_PIN 22
+#define FRAISE_DRV_LEVEL 1 // DRV is straight (drive when drv_pin is high)
+
---
+++ boards/pico_drv1/boardio.h
@@ -0,0 +1,4 @@
+#ifndef _PICO_IO_
+#define _PICO_IO_
+
+#endif // ifndef _PICO_IO_
